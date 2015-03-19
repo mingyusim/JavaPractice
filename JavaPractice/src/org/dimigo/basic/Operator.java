4578ea3dd502264ca88fc7d2774ef48c4f4d0374
wp132317 <mkn1123@naver.com>
2015-03-19 16:50:45 +0900

    ..

--- JavaPractice/src/org/dimigo/basic/Operator.java
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -5,10 +5,10 @@ public class Operator {
 	public static void main(String[] args) {
 		int a = 9;
 		int b = 10;
-		double h1 = 5.8;
-		double h2 = 5.4;
-		double s1 = (a+b) * h1 / 2;
-		double s2 = a * h2; 
+		double h1 = 5.8;					// 사다리꼴의 높이
+		double h2 = 5.4;					// 평행사변형의 높이
+		double s1 = (a+b) * h1 / 2;			// 사다리꼴의 넓이
+		double s2 = a * h2; 				// 평행사변형의 넓이
 		
 		
 		System.out.println("<< 도형 넓이 비교 >>");
@@ -20,5 +20,4 @@ public class Operator {
 		else
 			System.out.println("평행사변형이 사다리꼴 보다 " + (s2 - s1) + " 더 큽니다");  
 	}
-
 }
