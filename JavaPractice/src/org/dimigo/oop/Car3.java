70e03f548513f05c7a0a6bc2fa5a2cd6f4381933
wp132317 <mkn1123@naver.com>
2015-04-15 15:01:44 +0900

    일곱번째 실습과제 제출(3)

---
+++ JavaPractice/src/org/dimigo/oop/Car3.java
@@ -0,0 +1,73 @@
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ * 	 |_ car
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 4. 14.
+ * </pre>
+ *
+ * @author		: 심민규
+ * @version		: 1.0
+ */
+public class Car3 {
+		private String company;
+		private String model;
+		private String color;
+		private int maxSpeed;
+		private int price;
+		
+		public Car3() {
+			
+		}
+		
+		public Car3(String company, String model, String color, int maxSpeed, int price) {
+			this.company = company;
+			this.model = model;
+			this.color = color;
+			this.maxSpeed = maxSpeed;
+			this.price = price;
+		}
+		
+		public Car3(String Company, String model, String color, int maxSpeed) {
+			this(Company, model, color, maxSpeed, 0);
+		}
+		
+		public Car3(String Company, String model, String color) {
+			this(Company, model, color, 0);
+		}
+
+		public String getCompany() {
+			return company;
+		}
+		public String getModel() {
+			return model;
+		}
+		public String getColor() {
+			return color;
+		}
+		public int getMaxSpeed() {
+			return maxSpeed;
+		}
+		public int getPrice() {
+			return price;
+		}
+		
+//		public void setCompany(String company) {
+//			this.company = company;
+//		}
+//		public void setModel(String model) {
+//			this.model = model;
+//		}
+//		public void setColor(String color) {
+//			this.color = color;
+//		}
+//		public void setMaxSpeed(int maxSpeed) {
+//			this.maxSpeed = maxSpeed;
+//		}
+//		public void setPrice(int price) {
+//			this.price = price;
+//		}
+}
