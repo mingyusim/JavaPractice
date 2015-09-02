ba80bb411e4339f544352a3006bccb7ac5be5bfb
wp132317 <mkn1123@naver.com>
2015-09-02 12:12:28 +0900

    열다섯번째 실습과제 제출

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,27 @@
+package org.dimigo.abstractclass;
+
+public class SmartPhoneTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SmartPhone[] phones = {
+				new IPhone("IPhone 6", "애플", 700000),
+				new Galaxy("갤럭시 S6", "삼성", 650000)
+		};
+		
+		
+		for(SmartPhone phone : phones) {
+			System.out.println(phone);
+			phone.turnOn();
+			phone.pay();
+			
+			// IPhone 또는 Galaxy폰 기능 사용하기
+			phone.useSpecialFunction();
+		
+			phone.turnOff();
+			System.out.println();
+		}
+		
+	}
+
+}
