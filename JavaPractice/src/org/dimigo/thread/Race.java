4fe0f127b35afb85a6fd60d78cb848ad283922cb
wp132317 <mkn1123@naver.com>
2015-11-06 14:07:43 +0900

    스무번째 실습과제 제출

---
+++ JavaPractice/src/org/dimigo/thread/Race.java
@@ -0,0 +1,43 @@
+/**
+ * <pre>
+ * org.dimigo.thread
+ *   |_ Race
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 11. 6.
+ * </pre>
+ *
+ * @author		: 심민규
+ * @version		: 1.0
+ */
+package org.dimigo.thread;
+
+/**
+ * @author master
+ *
+ */
+public class Race {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("main thread start");
+		
+		Thread t1 = new Runner("홍길동");
+		Thread t2 = new Runner("홍길순");
+		
+		// MAX_PRIORITY(10), NORM_PRIORITY(5), MIN_PRIORITY
+		// 우선순위가 높은 스레드가 실행 기회를 더 많이 가진다.
+		t1.setPriority(Thread.MAX_PRIORITY);
+		t2.setPriority(Thread.MIN_PRIORITY);
+		
+		// multi thread로 실행하려면 start() 호출하기
+		t1.start();
+		t2.start();
+		
+		System.out.println("main thread stop");
+	}
+
+}
---
+++ JavaPractice/src/org/dimigo/thread/Runner.java
@@ -0,0 +1,47 @@
+/**
+ * <pre>
+ * org.dimigo.thread
+ *   |_ Runner
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 11. 6.
+ * </pre>
+ *
+ * @author		: 심민규
+ * @version		: 1.0
+ */
+package org.dimigo.thread;
+
+/**
+ * @author master
+ *
+ */
+public class Runner extends Thread{
+	String name;
+	
+	public Runner() {}
+	
+	public Runner(String name) {
+		this.name = name;
+	}
+	
+	public void run() {
+		System.out.println(name + " 출발");
+		
+		int i;
+		
+		for(i=100; i>=0; i-=10) {
+			System.out.println(name + " " + i + " 미터");
+			
+			try {
+					Thread.sleep(1000);
+			} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			if(i == 0) {
+				System.out.println(name + " 골인");
+			}
+		}
+	}
+}
